be0c490d86db803a154bb9e449fae51ec4e043da
navigator117 <navigator117@gmail.com>
2020-06-12 09:12:36 +0800

    release build

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-./configure --with-ssl=/opt/TASSL --prefix=/opt/CURL --enable-debug
+#!/bin/bash
+./configure --with-ssl=/opt/TASSL --prefix=/opt/CURL
+#./configure --with-ssl=/opt/TASSL --prefix=/opt/CURL --enable-debug
