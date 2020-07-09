8c553b9b4f89033762640acc4767ff1d43f4688b
navigator117 <navigator117@gmail.com>
2020-07-09 14:32:11 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 ./configure --with-ssl=/opt/TASSL --prefix=/opt/CURL
 #./configure --with-ssl=/opt/TASSL --prefix=/opt/CURL --enable-debug
 make
+read -s -n1 -p "press any key to install ..."
 sudo make install
